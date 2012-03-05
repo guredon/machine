7b190fcfd6e246a5b996fb1b9a76af03c3b99d98
gurene-don <e115715@ie.u-ryukyu.ac.jp>
2012-03-05 14:36:55 +0900

    correction yen >= 100

--- dispencer.rb
+++ dispencer.rb
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 
+
 ### お金を入れる ###
 
 puts'お金を入れて下さい'
+puts'¥100以上で変えます'
 
-yen = gets.chomp          # chompで、Enterを除くようにする
+yen = gets.chomp.to_i      # chompで、Enterを除くようにする
+                           # to_iで、整数として扱う
+sam = sam.to_i + yen
 
-while yen != '100'
-  yen = gets.chomp        # 文字として、認識してもらう必要がある
+while sam < 100
+  puts'もっと入れて下さい (´ω `)'
+  yen = gets.chomp.to_i        # 文字として、認識してもらう必要がある
+  sam = sam.to_i + yen
 end
 
 
 ### 飲み物を選択 ###
 
-puts'飲み物を選んで下さい'
+#if yen > 100
+
+puts'飲み物を入力して下さい'
 
 drink = gets.chomp
 
 puts 'どうぞ！' + drink.to_s + 'です'
+
+#else yen = gets.chomp.to_i      # chompで、Enterを除くようにする
+
+#end
 
 
 ### 当たりつき ###
