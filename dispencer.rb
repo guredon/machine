c0b8766dc676707407162562fa522c8e455a0818
gurene-don <e115715@ie.u-ryukyu.ac.jp>
2012-03-06 15:45:41 +0900

    余分なコメントを削除②

--- dispencer.rb
+++ dispencer.rb
@@ -9,12 +9,9 @@ puts'¥100以上で買えます'
 
 yen = yen.to_i + gets.chomp.to_i      # chompで、Enterを除くようにする
                                       # to_iで、整数として扱う
-#sam = sam.to_i + yen
-
 while yen < 100
   puts'もっと入れて下さい (´ω `)'
   yen = yen.to_i + gets.chomp.to_i
-  #sam = sam.to_i + yen
 end
 
 
