198e4c5bf3bb2da6c2333377f7791493c3dfc4c1
gurene-don <e115715@ie.u-ryukyu.ac.jp>
2012-03-06 15:37:20 +0900

    余分なコメントを削除

--- dispencer.rb
+++ dispencer.rb
@@ -3,33 +3,28 @@
 
 ### お金を入れる ###
 
+puts''
 puts'お金を入れて下さい'
 puts'¥100以上で買えます'
 
-yen = gets.chomp.to_i      # chompで、Enterを除くようにする
-                           # to_iで、整数として扱う
-sam = sam.to_i + yen
+yen = yen.to_i + gets.chomp.to_i      # chompで、Enterを除くようにする
+                                      # to_iで、整数として扱う
+#sam = sam.to_i + yen
 
-while sam < 100
+while yen < 100
   puts'もっと入れて下さい (´ω `)'
-  yen = gets.chomp.to_i        # 文字として、認識してもらう必要がある
-  sam = sam.to_i + yen
+  yen = yen.to_i + gets.chomp.to_i
+  #sam = sam.to_i + yen
 end
 
 
 ### 飲み物を選択 ###
-
-#if yen > 100
 
 puts'飲み物を入力して下さい'
 
 drink = gets.chomp
 
 puts 'どうぞ！' + drink.to_s + 'です'
-
-#else yen = gets.chomp.to_i      # chompで、Enterを除くようにする
-
-#end
 
 
 ### 当たりつき ###
