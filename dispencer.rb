ac8c685a2e1e692728c7ed063a735349bc652795
gurene-don <e115715@ie.u-ryukyu.ac.jp>
2012-03-07 00:32:57 +0900

    おつり

--- dispencer.rb
+++ dispencer.rb
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# puts'' は改行
 
-### お金を入れる ###
+##### お金を入れる #####
 
 puts''
 puts'お金を入れて下さい'
@@ -16,28 +17,50 @@ while yen < 100
 end
 
 
-### 飲み物を選択 ###
+##### 飲み物を選択 #####
 
+puts''
 puts'飲み物を入力して下さい'
 
 drink = gets.chomp
 
+puts''
 puts 'どうぞ！' + drink.to_s + 'です'
+puts''
+
+yen = yen - 100                       # 飲み物を買ったので、100円を引く 
 
 
-### 当たりつき ###
+##### 当たりつき #####
 
 a = (rand(10))
 
-if a == 7
+# 当たり（１回目）
+if a == 7 
+  
   puts'おめでとうございます(・∀ ・) 当たりました！もう１本選んで下さい'
   drink = gets.chomp
   puts 'どうぞ！' + drink.to_s + 'です'
+  puts''
   a = (rand(10))
-  if a == 7 
+
+
+# 当たり（２回目）
+  if a == 7
     puts'２連チャンｷﾀ━━━━ (ﾟ∀ ﾟ)━━━━ !! もう１本選んで下さい'
     drink = gets.chomp
     puts'どうぞ！' + drink.to_s + 'です'
+    puts''
   end
+end
+
+
+##### おつり #####
+
+if yen > 0
+
+  puts 'ありがとうございましたm(_ _)m　おつりは' + yen.to_s + '円です'
+  puts''
+
 end
 
